889f4cd3f8ffe1e479330a97258185add194bba7
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-08-30 15:06:54 +0530

    Utils Funciton

--- utils/uploadToDatabase.py
+++ utils/uploadToDatabase.py
@@ -1,9 +1,20 @@
 from contestScrapper import getAvailableContests
 from supabaseClient import get_client
+import unixToDateTime
+
 
 client = get_client()
-contests = getAvailableContests()
 
-for contest in contests:
-    data, count = client.table("Contests").insert({
-        "id": contest["id"], "name": contest["name"], "duration": contest["durationSeconds"], "start_time": contest["startTimeSeconds"]}).execute()
+
+def contestAlreadyExists(contestId):
+    data, count = client.table("Contests").select(
+        "id").eq("id", str(contestId)).execute()
+    return len(data[1]) != 0
+
+
+def uploadToDatabase():
+    contests = getAvailableContests()
+    for contest in contests:
+        if not contestAlreadyExists(contest["id"]):
+            data, count = client.table("Contests").insert({
+                "id": contest["id"], "name": contest["name"], "duration": contest["durationSeconds"], "start_time": unixToDateTime.unixToDateTime(contest["startTimeSeconds"])}).execute()
